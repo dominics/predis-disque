36c96d8f3c5b29a552a594a2ff67edefaa4e69d7
Dominic Scheirlinck <dominic@varspool.com>
2017-03-06 13:11:25 +0000

    Start on profiles

---
+++ test/Client/BasicTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Varspool\Disque\Client;
+
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function testClient()
+    {
+        $this->assertTrue(true, 'woop');
+    }
+}
