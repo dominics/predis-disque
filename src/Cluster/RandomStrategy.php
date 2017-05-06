03d2130ef365d3baebc50b3dff0424143ddee226
Dominic Scheirlinck <dominic@varspool.com>
2017-05-06 08:15:16 +0000

    Revamped the cluster strategy interface

--- src/Cluster/RandomStrategy.php
+++ src/Cluster/RandomStrategy.php
@@ -3,15 +3,35 @@
 namespace Predisque\Cluster;
 
 use Predis\Connection\NodeConnectionInterface;
+use Predisque\Connection\Parameters;
 
 class RandomStrategy implements StrategyInterface
 {
     /**
      * @param array $nodes
+     * @return Parameters
+     */
+    public function pickNodeFromHello(array $nodes): Parameters
+    {
+        // Don't allow nodes with priority higher than 99 (LEAVING)
+        $nodes = array_filter($nodes, function ($node) {
+            return (int)$node[3] <= 99;
+        });
+
+        $node = array_values($nodes)[random_int(0, count($nodes) - 1)];
+
+        return new Parameters([
+            'host' => $node[1],
+            'port' => $node[2],
+        ]);
+    }
+
+    /**
+     * @param array $connections
      * @return NodeConnectionInterface
      */
-    public function pickNode(array $nodes): NodeConnectionInterface
+    public function pickInitialConnection(array $connections): NodeConnectionInterface
     {
-        return $nodes[random_int(0, count($nodes) - 1)];
+        return array_values($connections)[random_int(0, count($connections) - 1)];
     }
 }
--- src/Cluster/StrategyInterface.php
+++ src/Cluster/StrategyInterface.php
@@ -3,12 +3,23 @@
 namespace Predisque\Cluster;
 
 use Predis\Connection\NodeConnectionInterface;
+use Predisque\Connection\Parameters;
 
 interface StrategyInterface
 {
     /**
-     * @param array $nodes
+     * Pick an initial connection to use (before making a HELLO request) from a set of connections
+     *
+     * @param array $connections
      * @return NodeConnectionInterface
      */
-    public function pickNode(array $nodes): NodeConnectionInterface;
+    public function pickInitialConnection(array $connections): NodeConnectionInterface;
+
+    /**
+     * Pick a set of connection parameters to use from a HELLO response
+     *
+     * @param array $nodes Response from HELLO: array of arrays, [[string $id, string $host, int $port, int $priority]]
+     * @return Parameters The connection parameters to use for the next connection to this cluster
+     */
+    public function pickNodeFromHello(array $nodes): Parameters;
 }
